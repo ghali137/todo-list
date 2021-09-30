13f076bdebd8b2542dc47b052e205b0b42904fd0
Ghali Bouchama <ghali.lecarre@gmail.com>
2021-09-30 12:35:12 +0100

    first

---
+++ dist/precache-manifest.36696024939dac4d28ae471efff3a2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fce4141e98cd99aab4b",
+    "url": "/css/app.ac659c73.css"
+  },
+  {
+    "revision": "42c472451f61125aaeb9a9572fc96584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fce4141e98cd99aab4b",
+    "url": "/js/app.e444021c.js"
+  },
+  {
+    "revision": "adf1d2616bfcdb7bca81",
+    "url": "/js/chunk-vendors.1ad922d2.js"
+  },
+  {
+    "revision": "33f7fa286bf61f74c8793938f3d7d99b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
